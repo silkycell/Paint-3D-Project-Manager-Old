eed05262f8ff9260c646f488fba7daaa11543a5e
Foxel <kegee55@gmail.com>
2023-06-15 14:22:46 -0400

    make cache keys be an enum

--- source/util/CacheManager.hx
+++ source/util/CacheManager.hx
@@ -4,38 +4,46 @@ import flixel.util.FlxColor;
 import openfl.display.BitmapData;
 import util.ProjectFileUtil.ProjectFile;
 
+enum CacheType
+{
+	THUMBNAIL;
+	COLOR;
+	SIZE;
+	OBJECTCOUNT;
+}
+
 class CacheManager
 {
-	static var cacheMap:Map<String, Map<ProjectFile, Any>> = new Map();
+	static var cacheMap:Map<CacheType, Map<ProjectFile, Any>> = new Map();
 
 	public static function initialize()
 	{
-		cacheMap['thumbnail'] = new Map<ProjectFile, BitmapData>();
-		cacheMap['color'] = new Map<ProjectFile, FlxColor>();
-		cacheMap['size'] = new Map<ProjectFile, Float>();
-		cacheMap['objectcount'] = new Map<ProjectFile, Float>();
+		cacheMap[THUMBNAIL] = new Map<ProjectFile, BitmapData>();
+		cacheMap[COLOR] = new Map<ProjectFile, FlxColor>();
+		cacheMap[SIZE] = new Map<ProjectFile, Float>();
+		cacheMap[OBJECTCOUNT] = new Map<ProjectFile, Float>();
 
 		ProjectFileUtil.defaultThumb = Assets.getBitmapData('assets/images/thumbFallback.png');
 	}
 
-	public static function getCachedItem(map:String, key:ProjectFile):Dynamic
+	public static function getCachedItem(map:CacheType, key:ProjectFile):Dynamic
 	{
 		return cacheMap[map][key];
 	}
 
-	public static function setCachedItem(map:String, key:ProjectFile, object:Dynamic):Dynamic
+	public static function setCachedItem(map:CacheType, key:ProjectFile, object:Dynamic):Dynamic
 	{
 		return cacheMap[map][key] = object;
 	}
 
-	public static function clearCachePool(map:String)
+	public static function clearCachePool(map:CacheType)
 	{
-		if (map == 'thumbnail')
+		if (map == THUMBNAIL)
 		{
 			// ACTUALLY remove the bitmaps from memory
 			var funnyCache:openfl.utils.AssetCache = cast openfl.utils.Assets.cache;
 			funnyCache.bitmapData = new Map<String, BitmapData>();
-			cacheMap['thumbnail'].clear();
+			cacheMap[THUMBNAIL].clear();
 
 			// make sure fallback thumbnail isn't deleted, though.
 			ProjectFileUtil.defaultThumb = Assets.getBitmapData('assets/images/thumbFallback.png');
